91bada0a68ff736a3145d52291d48ef2ad605373
ryan apratama <ryanapratama16@gmail.com>
2023-08-13 13:40:55 +0700

    move layouts

--- ui/src/pages/home.jsx
+++ ui/src/pages/home.jsx
@@ -1,7 +1,7 @@
 import VideoThumbnail from '../components/Fragments/VideoThumbnail';
 import Navbar from '../components/Fragments/Navbar'
 
-import VideoThumbnailsLayouts from '../components/layouts/VideoThumbnailsLayouts';
+import VideoThumbnailsLayouts from '../layouts/VideoThumbnailsLayouts'
 
 export default function HomePage() {
     return (
