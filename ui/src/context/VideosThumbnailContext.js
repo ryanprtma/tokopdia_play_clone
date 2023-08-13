13a2fcad10012e748a5b594cd64c7403b1759f42
ryan apratama <ryanapratama16@gmail.com>
2023-08-13 14:49:30 +0700

    add videos thumbnail context

---
+++ ui/src/context/VideosThumbnailContext.js
@@ -0,0 +1,38 @@
+import { createContext, useState, useEffect } from "react";
+
+export const VideosThumbnailContext = createContext();
+
+export const VideosThumbnailProvider = (props) => {
+    const [videos, setVideos] = useState([]);
+
+    const fetchWebApi = async () => {
+        try {
+            const apiUrl = "http://localhost:3000/api/videos";
+            const response = await fetch(apiUrl, {
+                method: 'GET'
+            });
+
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+
+            const results = await response.json();
+            setVideos(results.data.videos);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            return null;
+        }
+    }
+
+    useEffect(() => {
+        fetchWebApi();
+    }, []);
+
+    return (
+        <>
+            <VideosThumbnailContext.Provider value={
+                { videos, setVideos }
+            }>{props.children}</VideosThumbnailContext.Provider>
+        </>
+    )
+};
