c73e282ea7454c6bd78641ada7514447d24cd626
ryan apratama <ryanapratama16@gmail.com>
2023-08-14 21:53:02 +0700

    add routes

--- ui/src/routes/Routes.js
+++ ui/src/routes/Routes.js
@@ -1,18 +1,29 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes as Broutes, Route } from 'react-router-dom';
 import { VideosThumbnailProvider } from '../context/VideosThumbnailContext';
+import { VideoDetailProvider } from '../context/VideoDetailContext';
+import { CommentsProvider } from '../context/CommentsContext';
+import { ProductsProvider } from '../context/ProductsContext';
+
 import HomePage from '../pages/home';
+import VideoDetailPage from '../pages/videoDetail';
 
 const Routes = () => {
     return (
         <>
             <Router>
                 <VideosThumbnailProvider>
-                    <Switch>
-                        <Route path="/" exact>
-                            <HomePage />
-                        </Route>
-                    </Switch>
+                    <VideoDetailProvider>
+                        <CommentsProvider>
+                            <ProductsProvider>
+                                <Broutes>
+                                    <Route exact path="/play/channels" element={<HomePage />} />
+                                    <Route exact path="/play/channels/:id" element={<VideoDetailPage />} />
+                                </Broutes>
+                            </ProductsProvider>
+                        </CommentsProvider>
+                    </VideoDetailProvider>
                 </VideosThumbnailProvider>
+
             </Router>
         </>
     )
